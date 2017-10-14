c5d4cd669cf1fa2e85ffdb8ba0aa23166665ee8a
gugug <734093894@qq.com>
2017-10-14 21:12:03 +0800

    productDao

---
+++ src/main/java/www/gdufs/tmall/dao/IProductDao.java
@@ -0,0 +1,10 @@
+package www.gdufs.tmall.dao;
+
+import www.gdufs.tmall.bean.Product;
+
+/**
+ * Created by gu on 2017/10/14.
+ */
+public interface IProductDao {
+    Product get(int id);
+}
---
+++ src/main/java/www/gdufs/tmall/dao/impl/ProductDao.java
@@ -0,0 +1,14 @@
+package www.gdufs.tmall.dao.impl;
+
+import www.gdufs.tmall.bean.Product;
+import www.gdufs.tmall.dao.IProductDao;
+
+/**
+ * Created by gu on 2017/10/14.
+ */
+public class ProductDao implements IProductDao{
+    @Override
+    public Product get(int id) {
+        return null;
+    }
+}
