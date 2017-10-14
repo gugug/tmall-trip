55ea31550fb9a18b9a25b499be669d4aa3e8205e
gugug <734093894@qq.com>
2017-10-14 20:57:31 +0800

    ProductImage对应表的CRUD和非CRUD的操作接口和实现类

---
+++ src/main/java/www/gdufs/tmall/dao/IProductImageDao.java
@@ -0,0 +1,31 @@
+package www.gdufs.tmall.dao;
+
+import www.gdufs.tmall.bean.ProductImage;
+
+import java.util.List;
+
+/**
+ * Created by gu on 2017/10/14.
+ */
+public interface IProductImageDao {
+    //1. 增加
+    void add(ProductImage bean);
+
+    //2. 删除
+    void delete(int id);
+
+    //3. 修改
+    void update(ProductImage bean);
+
+    //4. 根据id获取
+    ProductImage get(int id);
+
+    //5. 分页查询
+    List<ProductImage> list(int start, int count);
+
+    //6. 查询所有
+    List<ProductImage> list();
+
+    //7. 获取总数
+    int getTotal();
+}
---
+++ src/main/java/www/gdufs/tmall/dao/impl/ProductImageDao.java
@@ -0,0 +1,46 @@
+package www.gdufs.tmall.dao.impl;
+
+import www.gdufs.tmall.bean.ProductImage;
+import www.gdufs.tmall.dao.IProductImageDao;
+
+import java.util.List;
+
+/**
+ * Created by gu on 2017/10/14.
+ */
+public class ProductImageDao implements IProductImageDao{
+    @Override
+    public void add(ProductImage bean) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(ProductImage bean) {
+
+    }
+
+    @Override
+    public ProductImage get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<ProductImage> list(int start, int count) {
+        return null;
+    }
+
+    @Override
+    public List<ProductImage> list() {
+        return null;
+    }
+
+    @Override
+    public int getTotal() {
+        return 0;
+    }
+}
